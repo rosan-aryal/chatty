814e37f5002b495855254937ea54cafb974ee7ab
Escobar <roshanjr464@gmail.com>
2026-02-21 22:41:50 +0545

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@chat-application/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@chat-application/db";
+import * as schema from "@chat-application/db/schema/auth";
+import { env } from "@chat-application/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
